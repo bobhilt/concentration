51d9b1ec9d90a9522cdac8f9ce938ec58663b1d2
Bob Hiltner <bobhilt@gmail.com>
2019-02-10 23:46:20 -1000

    Implement event handler, turn eveluation and handling.

--- concentration.js
+++ concentration.js
@@ -1,20 +1,52 @@
-const card_backs = [];
+const card_backs = ['a', 'b', 'b', 'a'];
+const turn = [];
+var turnCount = 0;
+
+const card_side = [0,0,0,0]; // 0, 1 for front, back
 
 var cards = document.getElementsByClassName('card');
 
-function toggleCard(card) {
-    if (card.innerHTML == "clicked") {
-        card.innerHTML = card.id;
-    } else {
-        card.innerHTML = "clicked";
-    }    
+function getCardIndex(card) {
+    return Number(card.id.slice(-3));
 }
 
+function toggleCard(card) {
+    // ToDo: Front and back images
+    let cardIndex = getCardIndex(card);
 
-var cardClickEventHandler = function() {
-    // var attribute = this.getAttribute("data-myattribute");
+    if (card_side[cardIndex]) {
+        card.innerHTML = card.id;
+    } else {
+        card.innerHTML = card_backs[cardIndex];
+    }
+    card_side[cardIndex] = 1 - card_side[cardIndex];
+}
+function isCorrectTurn() {
+    let card0 = turn[0];
+    let card1 = turn[1];
+    let card0Index = getCardIndex(card0);
+    let card1Index = getCardIndex(card1);
+    return card_backs[card0Index] === card_backs[card1Index];
+}
+
+function resolveTurn(isCorrect) {
+    if (isCorrect) {
+        alert('correct');
+    } else {
+        alert('incorrect');
+    }
+    toggleCard(turn[0]);
+    toggleCard(turn[1]);
+    turn.pop();
+    turn.pop();
+}
+const cardClickEventHandler = function() {
+    turnCount++;
+    turn.push(this);
     toggleCard(this);
-    console.log(this.innerHTML);
+    if (turn.length == 2) {
+        resolveTurn(isCorrectTurn()); 
+    } 
 };
 
 Array.from(cards).forEach(function(card) {
